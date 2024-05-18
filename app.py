56fcc40a59a10eae3da0b37bd75cfa52a9563063
Norah Ariokot <ariokot.norah109@gmail.com>
2024-05-18 14:14:17 +0300

    feat: adding user reg details in database

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import datetime
 import os
 
 from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session, jsonify
+from flask import Flask, flash, redirect, render_template, request, g, session, jsonify
 from flask_session import Session
 from dotenv import load_dotenv
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -16,14 +16,19 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv("app_secret_key")
 
 
-
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///movies.db")
+db = SQL("sqlite:///movie_lib.db")
+
+
+@app.before_request
+def before_request():
+    g.db = db
+
 
 @app.route("/")
 def index():
@@ -36,12 +41,21 @@ def login():
 
 @app.route("/create_account", methods=["GET","POST"])
 def create_account():
-    print("Reg form submitted")
+    #print("Reg form submitted")
     reg_form = CreateUserForm()
-    print(reg_form)
+   
+    # Ensure all user account registration is validated and form submitted via POST request method
     if reg_form.validate_on_submit():
-        reg_form
-        print("Form validated")
+        firstname = reg_form.first_name.data
+        lastname = reg_form.last_name.data
+        username = reg_form.user_name.data
+        password = reg_form.password.data
+        hash = generate_password_hash(password)
+
+        # user registration data stored in users table in movie_lib db
+        db.execute("INSERT INTO users (first_name, last_name, user_name, hash) VALUES (?,?,?,?)", firstname, lastname, username, hash)
+
+        # user is then redirected to the login page
         return redirect('/')
     return render_template("create_account.html", reg_form=reg_form)
 
--- forms.py
+++ forms.py
@@ -1,25 +1,25 @@
 import re
 
 from flask_wtf import FlaskForm
+from flask import g
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import EqualTo, InputRequired, Length, ValidationError
 from helpers import current_names
 
-
+# Custom validator to ensure the last and first names of users only contain letters
 def name_validator(form, field):
     name = field.data
     if not re.search(r'[a-zA-Z]', name):
         raise ValidationError('Name must contain only letters')
 
-
+# Custom validator to ensure that user names are unique for every user
 def user_name_validator(form, field):
-    users = current_names
     user_name = field.data
-    for user in users:
-        if user == user_name:
-            raise ValidationError('User name already exists, please use another one.')
+    existing_username = g.db.execute("SELECT * FROM users WHERE user_name = ?", user_name)
+    if existing_username:
+        raise ValidationError('User name already exists, please use another one.')
 
-
+# Custom validators for passwords
 def password_validator(form, field):
     password = field.data
     if not re.search(r'\d', password):
@@ -39,3 +39,5 @@ class CreateUserForm(FlaskForm):
     user_name = StringField('User Name', validators=[InputRequired(), user_name_validator])
     password = PasswordField('Password', validators=[InputRequired(), EqualTo('verify_password', message="Passwords do no match"), Length(min=8, message='Password must be atleast 8 characters long'), password_validator])
     verify_password = PasswordField('Repeat Password', validators=[InputRequired(message='Please repeat the new password')])
+
+
